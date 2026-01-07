35169f59d0edce3387ec8e6532903d88821cf7c0
power-datebase <panzzhub@gmail.com>
2026-01-08 01:04:04 +0700

    mount-list.lua

--- mount-list.lua
+++ mount-list.lua
@@ -1,5 +1,5 @@
 return {
-    {Name = "velora", Link = "https://raw.githubusercontent.com/Abangapeng/FREE-VELORA/refs/heads/main/VIP-velora.lua,
+    {Name = "velora", Link = "https://raw.githubusercontent.com/Abangapeng/FREE-VELORA/refs/heads/main/VIP-velora.lua"},
     {Name = "Nameless Admin", Link = "https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"},
     {Name = "Fates Admin", Link = "https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"},
     {Name = "Remote Spy", Link = "https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"},
