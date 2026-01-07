2c113ccaa654fdf10757baf0c1cec7587eb56668
power-datebase <panzzhub@gmail.com>
2026-01-07 23:31:42 +0700

    mount-list.lua

---
+++ mount-list.lua
@@ -0,0 +1,8 @@
+return {
+    {Name = "Infinite Yield", Link = "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"},
+    {Name = "Nameless Admin", Link = "https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"},
+    {Name = "Fates Admin", Link = "https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"},
+    {Name = "Remote Spy", Link = "https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"},
+    {Name = "Dark Dex V3", Link = "https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua"},
+    -- Kamu bisa update list ini kapan saja lewat GitHub tanpa ubah script utama
+}
