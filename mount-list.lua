6c3e9448690fcda0e6cee657b7006804efd62bbb
power-datebase <panzzhub@gmail.com>
2026-01-13 23:25:33 +0700

    mount-list.lua

--- mount-list.lua
+++ mount-list.lua
@@ -1,8 +1,3 @@
 return {
-    {Name = "velora", Link = "https://raw.githubusercontent.com/Abangapeng/FREE-VELORA/refs/heads/main/VIP-velora.lua"},
-    {Name = "Nameless Admin", Link = "https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"},
-    {Name = "Fates Admin", Link = "https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"},
-    {Name = "Remote Spy", Link = "https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"},
-    {Name = "Dark Dex V3", Link = "https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua"},
-    -- Kamu bisa update list ini kapan saja lewat GitHub tanpa ubah script utama
+    {Name = "mount Velora", Link = "https://raw.githubusercontent.com/Abangapeng/FREE-VELORA/refs/heads/main/VIP-velora.lua"},
 }
