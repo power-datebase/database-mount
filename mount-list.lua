d7266757a89997c23aad41021c8e1e30953f9520
power-datebase <panzzhub@gmail.com>
2026-01-14 14:05:17 +0700

    mount-list.lua

--- mount-list.lua
+++ mount-list.lua
@@ -1,3 +1,5 @@
 return {
     {Name = "mount Velora", Link = "https://raw.githubusercontent.com/Abangapeng/FREE-VELORA/refs/heads/main/VIP-velora.lua"},
+{Name = "mount yahayuk", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/yahayuk.lua"},
 }
+
