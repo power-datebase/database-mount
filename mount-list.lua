3f3e1a13623af2ce1d7c4efb102a3c22e2284590
power-datebase <panzzhub@gmail.com>
2026-01-16 14:26:08 +0700

    Update mount-list.lua

--- mount-list.lua
+++ mount-list.lua
@@ -1,6 +1,6 @@
 return {
     {Name = "Mount Velora ", Link = "https://raw.githubusercontent.com/Abangapeng/FREE-VELORA/refs/heads/main/VIP-velora.lua"},
-    {Name = "Mount Aktif ", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/aktif.lua.lua"},
+    {Name = "Mount Aktif ", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/aktif.lua"},
 {Name = "Mount Freestyle ", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/freestyle.lua"},
     {Name = "Mount Elystra ", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/Elystra.lua"},
     {Name = "Mount Yahayuk  ", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/yahayuk.lua"},
