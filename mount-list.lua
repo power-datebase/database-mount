ed8327186ef37b67d2fbf01f8eb433b99e23d4a5
power-datebase <panzzhub@gmail.com>
2026-01-16 14:23:20 +0700

    mount-list.lua

--- mount-list.lua
+++ mount-list.lua
@@ -1,8 +1,9 @@
 return {
     {Name = "Mount Velora ", Link = "https://raw.githubusercontent.com/Abangapeng/FREE-VELORA/refs/heads/main/VIP-velora.lua"},
+    {Name = "Mount Aktif ", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/aktif.lua.lua"},
 {Name = "Mount Freestyle ", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/freestyle.lua"},
     {Name = "Mount Elystra ", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/Elystra.lua"},
-    {Name = "Mount Yahayuk 🔥 ", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/yahayuk.lua"},
+    {Name = "Mount Yahayuk  ", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/yahayuk.lua"},
     {Name = "Mount tranggulasih ", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/tranggulasih.lua"},
 
     
