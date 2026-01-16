53c1c1656c9e409a89876d9cb159bf727d027fab
power-datebase <panzzhub@gmail.com>
2026-01-16 10:07:27 +0700

    mount-list.lua

--- mount-list.lua
+++ mount-list.lua
@@ -1,6 +1,6 @@
 return {
-    {Name = "Mount Velora 🔥", Link = "https://raw.githubusercontent.com/Abangapeng/FREE-VELORA/refs/heads/main/VIP-velora.lua"},
-{Name = "Mount Freestyle 🔥", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/freestyle.lua"},
-    {Name = "Mount Velora 🔥", Link = "https://raw.githubusercontent.com/Abangapeng/FREE-VELORA/refs/heads/main/VIP-velora.lua"},
+    {Name = "Mount Velora ", Link = "https://raw.githubusercontent.com/Abangapeng/FREE-VELORA/refs/heads/main/VIP-velora.lua"},
+{Name = "Mount Freestyle ", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/freestyle.lua"},
+    {Name = "Mount Elystra ", Link = "https://raw.githubusercontent.com/power-datebase/list-mount/refs/heads/main/Elystra.lua"},
 }
 
